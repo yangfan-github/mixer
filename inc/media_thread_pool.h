5995cf749a6905f15eaaa8529b641cdeef9b9a9b
杨帆 <yangfan57331@offcn.com>
2019-11-27 14:44:34 +0800

    :add thread pool

---
+++ inc/media_thread_pool.h
@@ -0,0 +1,41 @@
+#ifndef MEDIA_THREAD_POOL_H
+#define MEDIA_THREAD_POOL_H
+#include <map>
+
+#include <thread>
+#include <boost/asio.hpp>
+
+#include "media_frame.h"
+
+
+struct media_task
+{
+
+ERROR_CODE_BEG(200)
+    rc_again = rc_base,
+    rc_eof,
+ERROR_CODE_END
+    bool _is_live;
+    int64_t _time;
+    virtual ret_type process() = 0;
+    media_task();
+};
+
+class media_thread_pool
+{
+        typedef boost::asio::io_context::executor_type ExecutorType;
+    protected:
+        boost::asio::io_context _ioc;
+        boost::asio::executor_work_guard<ExecutorType> _work_guard;
+        vector<std::thread> _threads;
+    public:
+        media_thread_pool(size_t count_thread = 0);
+        virtual ~media_thread_pool();
+        boost::asio::io_context& get_context();
+        ret_type post(media_task* task);
+        ret_type process(media_task* task);
+};
+
+extern media_thread_pool g_pool;
+
+#endif // MEDIA_THREAD_POOL_H
