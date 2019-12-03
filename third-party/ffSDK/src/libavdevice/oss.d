204398ff88ce1c2d7cb09fbcab4ea6cc0d95eb81
杨帆 <yangfan57331@offcn.com>
2019-12-03 16:40:52 +0800

    :add ffSDK

---
+++ third-party/ffSDK/src/libavdevice/oss.d
@@ -0,0 +1,16 @@
+libavdevice/oss.o: libavdevice/oss.c config.h libavutil/log.h \
+ libavutil/avutil.h libavutil/common.h libavutil/attributes.h \
+ libavutil/macros.h libavutil/version.h libavutil/avconfig.h \
+ libavutil/intmath.h libavutil/x86/intmath.h libavutil/mem.h \
+ libavutil/error.h libavutil/internal.h libavutil/timer.h libavutil/log.h \
+ libavutil/x86/timer.h libavutil/cpu.h libavutil/dict.h \
+ libavutil/pixfmt.h libavutil/x86/emms.h libavutil/attributes.h \
+ libavutil/cpu.h libavutil/libm.h libavutil/intfloat.h \
+ libavutil/mathematics.h libavutil/rational.h libavcodec/avcodec.h \
+ libavutil/samplefmt.h libavutil/avutil.h libavutil/buffer.h \
+ libavutil/channel_layout.h libavutil/dict.h libavutil/frame.h \
+ libavutil/buffer.h libavutil/samplefmt.h libavutil/pixfmt.h \
+ libavutil/rational.h libavcodec/version.h libavutil/version.h \
+ libavdevice/avdevice.h libavdevice/version.h libavutil/opt.h \
+ libavformat/avformat.h libavformat/avio.h libavutil/common.h \
+ libavformat/version.h libavdevice/oss.h
---
+++ third-party/ffSDK/src/libavdevice/xcbgrab.d
@@ -0,0 +1,17 @@
+libavdevice/xcbgrab.o: libavdevice/xcbgrab.c config.h \
+ libavutil/internal.h libavutil/attributes.h libavutil/timer.h \
+ libavutil/log.h libavutil/avutil.h libavutil/common.h libavutil/macros.h \
+ libavutil/version.h libavutil/avconfig.h libavutil/intmath.h \
+ libavutil/x86/intmath.h libavutil/mem.h libavutil/error.h \
+ libavutil/internal.h libavutil/rational.h libavutil/mathematics.h \
+ libavutil/intfloat.h libavutil/pixfmt.h libavutil/x86/timer.h \
+ libavutil/cpu.h libavutil/dict.h libavutil/x86/emms.h \
+ libavutil/attributes.h libavutil/cpu.h libavutil/libm.h \
+ libavutil/mathematics.h libavutil/opt.h libavutil/samplefmt.h \
+ libavutil/parseutils.h libavutil/time.h libavformat/avformat.h \
+ libavcodec/avcodec.h libavutil/samplefmt.h libavutil/avutil.h \
+ libavutil/buffer.h libavutil/channel_layout.h libavutil/dict.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/log.h libavutil/pixfmt.h \
+ libavutil/rational.h libavcodec/version.h libavutil/version.h \
+ libavformat/avio.h libavutil/common.h libavformat/version.h \
+ libavformat/internal.h libavformat/avformat.h libavformat/os_support.h
